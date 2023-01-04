7f825378aa74af16681b8f208cce1a116e5d2715
Thomas Jindu <thomas@spectrumits.co.zw>
2023-01-04 09:42:43 +0200

    initial commit

---
+++ src/context.js
@@ -0,0 +1,52 @@
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [location, setLocation] = useState({});
+  const [page, setPage] = useState({page: '', links: []});
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = (text, coordinates) => {
+    const currentPage = sublinks.find(obj => obj.page === text);
+    setPage(currentPage)
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+        location,
+        page
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
